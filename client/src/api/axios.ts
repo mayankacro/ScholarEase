2e5a7672ef1a8426cfaf7841fd2d293846d7aa5c
mayankacro <183179696+mayankacro@users.noreply.github.com>
2026-08-18 18:38:43 +0530

    Add JWT authentication flow

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,8 +2,17 @@ import axios from "axios";
 
 //Axios ek JavaScript library hai jo frontend se HTTP requests bhejne ke kaam aati hai.
 
-const api = axios.create({
+const api = axios.create({ //Hum apna Axios ka customized instance bana rahe hain.
     baseURL: "http://localhost:5000",
-})
+});
+
+api.interceptors.request.use((config) => {  //interceptor => Jab bhi api se request jaane wali ho, usse pehle mere paas aao.
+    const token = localStorage.getItem("token");
+
+    if(token){ //Agar token hai, request ke headers mein daal do.
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 export default api;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,22 +5,47 @@ function Login(){
     // useSate() --> React ka ek Hook hai jo component ke andar data ko remember/state mein rakhne deta hai.
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
     const handleLogin = async (e: any) => {
         e.preventDefault(); //Email likho Password likho Login click karo Console check karo Page reload nahi hona chahiye
         
+        setError("");// supose wrong password fir invalid credential phir user password correct karta hai. To purana error screen se hatna chahiye.
+
+        try{
         const res = await api.post("/api/auth/login", {
-            email: email,
-            password: password
+            email,
+            password,
         });
-        console.log(res.data)
-    
-    };
+        console.log(res.data);
+
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+
+    } catch (error: any){
+        setError(
+            error.response?.data?.message || "Login failed"
+        );
+
+    }
+};
+
+const getProfile = async () => {
+    try {
+        const res = await api.get("/api/auth/profile");
+        console.log("Profile:", res.data)
+    } catch(error: any){
+        console.log("Profile failed:",
+            error.response?.data?.message || "Profile failed"
+        );
+    }
+};
 
     return (
         <form onSubmit={handleLogin}>
 
-        
+            {error && <p>{error}</p>} 
+
             <input
               type="email"
               required
@@ -39,6 +64,10 @@ function Login(){
 
             <button type="submit"> 
                 Login
+            </button>
+
+            <button type="button" onClick={getProfile}>
+                Get Profile
             </button>
 
               <p> Tu ne likha: {email}</p>
